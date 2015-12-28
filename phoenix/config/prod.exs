98c1e37b964c76f7534111087df58d8f516a744d
Andreas Tasch <andy.tasch@gmail.com>
2015-12-29 00:04:27 +0100

    Fix hostname.

--- phoenix/config/prod.exs
+++ phoenix/config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pete_phoenix, PetePhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "pete_phoenix.pi", port: 80],
+  url: [host: "pete-phoenix.pi", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
